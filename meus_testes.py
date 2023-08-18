7df9e39a86d2968fe9f3c9ccab4f12e50d6aba0a
Marlysson Prog <designprogthe@gmail.com>
2023-08-18 20:37:18 -0300

    Modificação_teste

--- meus_testes.py
+++ meus_testes.py
@@ -1,16 +1,24 @@
 import pytest
 
+
 def multiplicacao(x, y):
     return x*y
+
 
 def teste_multiplicacao1():
     assert multiplicacao(10, 2) == 20
 
+
 def teste_multiplicacao2():
     assert multiplicacao(9, 3) == 27
 
+
 def teste_multiplicacao3():
     assert multiplicacao(10, 9) == 90
+
+
+def teste_multiplicacao4():
+    assert multiplicacao(1, 5) == 5
 
 
 #                Executável        código python
